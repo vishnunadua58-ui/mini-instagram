11257515d76bc545fedbe4f31b393c3f5c71fb3a
vishnunadua58-ui <vishnunadua58@gmail.com>
2026-09-19 00:05:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,4 @@ def add_post():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-<div class="nav-bar"> <a href="#">Reel</a> <a href="#">Like</a> <a href="#">Profile</a> </div>.
 @app.route('/reel') def reel():return "Reels Page"
